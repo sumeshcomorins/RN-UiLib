e57d27ddb3d2daf825d1553457b069905e1abd02
sumecomorins <sumesh@comorins.com>
2022-05-19 22:20:18 +0530

    three tab and screens

--- src/screens/TabThree.js
+++ src/screens/TabThree.js
@@ -1,10 +1,150 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import _ from 'lodash';
+import {ScrollView, StyleSheet, Alert} from 'react-native';
+import {Avatar, AvatarHelper, View, Text, Colors, Typography} from 'react-native-ui-lib'; //eslint-disable-line
+
+
+const star = require('../../assets/icons/star.png');
+const onlineColor = Colors.$backgroundSuccessHeavy;
+const examples = [
+  {title: 'Custom Background', backgroundColor: Colors.violet60},
+  {title: 'Empty Avatar with ribbon', ribbonLabel: 'New'},
+  {
+    title: 'Initials with Color',
+    label: 'AD',
+    backgroundColor: Colors.$backgroundWarning,
+    labelColor: Colors.$textMajor,
+    ribbonLabel: 'New',
+    ribbonStyle: {backgroundColor: Colors.purple30}
+  },
+  {title: 'Initials, badge ("online")', label: 'ES', badgeProps: {backgroundColor: onlineColor}},
+  {
+    title: 'Image, badge ("away")',
+    source: {
+      uri:
+        'https://lh3.googleusercontent.com/-cw77lUnOvmI/AAAAAAAAAAI/AAAAAAAAAAA/WMNck32dKbc/s181-c/104220521160525129167.jpg'
+    },
+    badgeProps: {size: 10, backgroundColor: Colors.$backgroundWarningHeavy},
+    badgePosition: 'BOTTOM_RIGHT'
+  },
+
+  {
+    title: 'Smaller size, Badge ("offline")',
+    size: 40,
+    source: {
+      uri:
+        'https://lh3.googleusercontent.com/-CMM0GmT5tiI/AAAAAAAAAAI/AAAAAAAAAAA/-o9gKbC6FVo/s181-c/111308920004613908895.jpg'
+    },
+    badgeProps: {size: 10, backgroundColor: Colors.$backgroundDisabled},
+    badgePosition: 'BOTTOM_LEFT'
+  },
+  {
+    title: 'Image with fade in animation',
+    size: 60,
+    animate: true,
+    imageProps: {animationDuration: 1000},
+    source: {uri: 'https://static.pexels.com/photos/60628/flower-garden-blue-sky-hokkaido-japan-60628.jpeg'}
+  },
+  {
+    title: 'Big pimple',
+    size: 70,
+    backgroundColor: 'red',
+    source: {
+      uri: 'https://randomuser.me/api/portraits/women/24.jpg'
+    },
+    badgeProps: {size: 14, borderWidth: 0, backgroundColor: onlineColor},
+    badgePosition: 'TOP_LEFT'
+  },
+  {
+    title: 'Icon badge',
+    size: 60,
+    source: {
+      uri: 'https://randomuser.me/api/portraits/women/24.jpg'
+    },
+    badgeProps: {
+      icon: star,
+      size: 14,
+      borderWidth: 1.5,
+      borderColor: Colors.$outlineInverted,
+      iconStyle: {backgroundColor: Colors.$backgroundWarningHeavy}
+    }
+  },
+  {
+    title: 'GIF',
+    size: 48,
+    source: {
+      uri: 'https://media.giphy.com/media/3oEdv8elIVRa3daNl6/giphy.gif'
+    }
+  },
+  {
+    title: 'Invalid Gravatar (see logs)',
+    label: '🤦',
+    backgroundColor: Colors.$backgroundNeutralMedium,
+    source: {uri: 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=404'},
+    onImageLoadStart: () => console.log('AvatarScreen: Invalid avatar load STARTED...'), // eslint-disable-line
+    onImageLoadEnd: () => console.log('AvatarScreen: Invalid avatar load ENDED'), // eslint-disable-line
+    onImageLoadError: () => console.log('AvatarScreen: Invalid avatar load FAILED') // eslint-disable-line
+  },
+  {
+    title: 'Monitored Avatar (see logs)',
+    label: '?!',
+    backgroundColor: Colors.$backgroundGeneralHeavy,
+    source: {uri: 'https://static.altomusic.com/media/catalog/product/M/A/MAJ100SBK_0.jpg'},
+    onImageLoadStart: () => console.log('AvatarScreen: Monitored avatar load STARTED...'), // eslint-disable-line
+    onImageLoadEnd: () => console.log('AvatarScreen: Monitored avatar load ENDED') // eslint-disable-line
+  },
+  {
+    title: 'Empty Gravatar',
+    backgroundColor: Colors.$backgroundDanger,
+    source: {uri: 'https://www.gravatar.com/avatar/2497473d558a37020c558bf26e380a7c?d=blank'}
+  },
+  {
+    title: 'With custom badge label',
+    label: 'LD',
+    backgroundColor: Colors.$backgroundDanger,
+    badgePosition: 'BOTTOM_RIGHT',
+    badgeProps: {
+      label: '+2',
+      size: 24,
+      borderWidth: 1.5,
+      borderColor: Colors.$outlineInverted
+    }
+  }
+];
+
 
 export default function TabThree() {
+ const onAvatarPress = (item) => {
+    const {title, source, label} = item;
+    const uri = _.get(source, 'uri');
+    const isGravatar = !!uri && AvatarHelper.isGravatarUrl(uri);
+    const patchedGravatar = isGravatar ? AvatarHelper.patchGravatarUrl(uri) : undefined;
+    const message = `Label: ${label}\n\nImage-source: ${uri}\n\nIs Gravatar: ${isGravatar}\n\n${
+      patchedGravatar ? `Patched-uri: ${patchedGravatar}` : ''
+    }`;
+    Alert.alert(title, message);
+  }
   return (
-    <View>
-      <Text>TabThree</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+        {_.map(examples, (example, i) => (
+          <View key={i} style={styles.section}>
+            <Text $textDefault style={{...Typography.text80}}>{example.title}</Text>
+            <Avatar containerStyle={{marginVertical: 5}} {...example} onPress={() => onAvatarPress(example)}/>
+          </View>
+        ))}
+      </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 25,
+    backgroundColor: Colors.$backgroundDefault
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 15
+  }
+});
--- src/screens/TabTwo.js
+++ src/screens/TabTwo.js
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import {ScrollView, StyleSheet, ActivityIndicator} from 'react-native';
+import _ from 'lodash';
+import {AnimatedImage, Colors} from 'react-native-ui-lib';
 
 export default function TabTwo() {
+  const SampleImages = [
+    'https://static.pexels.com/photos/50721/pencils-crayons-colourful-rainbow-50721.jpeg',
+    'https://static.pexels.com/photos/60628/flower-garden-blue-sky-hokkaido-japan-60628.jpeg',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200'
+  ];
   return (
-    <View>
-      <Text>TabTwo</Text>
-    </View>
+    <ScrollView style={styles.container}>
+        {_.map(SampleImages, (image, index) => (
+          <AnimatedImage
+            containerStyle={{backgroundColor: Colors.blue50, marginBottom: 10}}
+            style={{resizeMode: 'cover', height: 250}}
+            source={{uri: image}}
+            loader={<ActivityIndicator />}
+            key={index}
+            animationDuration={index === 0 ? 300 : 800}
+          />
+        ))}
+      </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
